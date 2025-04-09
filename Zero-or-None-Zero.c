0d57df9e4467f0cf9f44bef59b374292bab1bafd
Sibgatullah <sibgatullah881@gmail.com>
2025-04-10 05:05:58 +0600

    zero or non zero

---
+++ Zero-or-None-Zero.c
@@ -0,0 +1,16 @@
+//In this problem you will be given an integer number N. Print "Zero" if the number is equals to 0 and "Non Zero" Otherwise.
+
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    if (n == 0)
+    {
+        printf("Zero");
+    }
+    else{
+        printf("Non Zero");
+    }
+    
+    return 0;
+}
