156b393a0745577a4054ff69483bb8533b9e0d17
Sibgatullah <sibgatullah881@gmail.com>
2025-04-10 05:10:25 +0600

    5 Marks for good hand writing

---
+++ 5 Marks for good hand writing.c
@@ -0,0 +1,10 @@
+//In this problem you will be given an integer number N. You will have to add 5 with N and print it.
+
+#include <stdio.h>
+int main(){
+    int n ,m ;
+    scanf("%d", &n);
+    m = n+5;
+    printf("%d",m);
+    return 0;
+}
