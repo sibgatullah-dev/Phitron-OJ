4e981b915a8bf72ad04b23ec541e906c25bb595c
Sibgatullah <sibgatullah881@gmail.com>
2025-04-10 18:02:06 +0600

    Sum of Two Numbers

---
+++ Sum of Two Numbers.c
@@ -0,0 +1,10 @@
+//Take two integers A and B as input and output their summation.
+
+#include<stdio.h>
+int main(){
+    int A, B, sum;
+    scanf("%d %d",&A,&B);
+    sum = A + B;
+    printf("%d",sum);
+    return 0;
+}
