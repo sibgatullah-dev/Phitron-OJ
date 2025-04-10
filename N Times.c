f87ebb9987c0a26385763da3b1a0da4d1d33c888
Sibgatullah <sibgatullah881@gmail.com>
2025-04-10 18:21:10 +0600

    N Times

---
+++ N Times.c
@@ -0,0 +1,13 @@
+//You will be given a positive integer N, you need to print "I Love Practice" N times.
+
+#include <stdio.h>
+int main(){
+    int N;
+    scanf("%d",&N);
+    for (int i = 1; i <=N; i++)
+    {
+        printf("I Love Practice\n");
+    }
+    
+    return 0;
+}
